f435464c212f445b2f2f20a93cf2f21ff08d77d6
l150531 <l1505@126.com>
2018-08-23 21:58:57 +0800

    添加日期显示

--- ChangeCss.js
+++ ChangeCss.js
@@ -1,5 +1,9 @@
 $(function() {
     $('#click_event4').click(function(){
+        var Date_3=$("#BBRQ").val();
+        
+        //数据填充在table表中
+        $("#Date_3").text(Date_3);
         var Out_Name_3=$("#Out_Name").val();
         var In_Name_3=$("#In_Name").val();
         
