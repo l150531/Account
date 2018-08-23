e93d5232691bc50be30bb0e825a389f57635da19
l150531 <l1505@126.com>
2018-08-23 21:55:58 +0800

    打印内容已全部修改完毕，剩下字体位置、字体、大小还未完成

--- ChangeCss.js
+++ ChangeCss.js
@@ -5,26 +5,39 @@ $(function() {
         
         //数据填充在table表中
         $("#Out_Name_3").text(Out_Name_3);
-        $("#In_Name_3").text(Out_Name_3);
+        $("#In_Name_3").text(In_Name_3);
 
         var Out_Accounted_3=$("#Out_Accounted").val();
         var In_Accounted_3=$("#In_Accounted").val();
-        $("#Out_Accounted_3").text(Out_Name_3);
-        $("#In_Accounted_3").text(Out_Name_3);
+        $("#Out_Accounted_3").text(Out_Accounted_3);
+        $("#In_Accounted_3").text(In_Accounted_3);
 
         var Out_Bank_3=$("#Out_Bank").val();
         var In_Bank_3=$("#In_Bank").val();
-        $("#Out_Bank_3").text(Out_Name_3);
-        $("#In_Bank_3").text(Out_Name_3);
+        $("#Out_Bank_3").text(Out_Bank_3);
+        $("#In_Bank_3").text(In_Bank_3);
 
         var Use_3=$("#Use").val();
         var Add_Info_3=$("#Add_Info").val();
-        $("#Use_3").text(Out_Name_3);
-        $("#Add_Info_3").text(Out_Name_3);
+        $("#Use_3").text(Use_3);
+        $("#Add_Info_3").text(Add_Info_3);
+        
+        var Use_3=$("#Use").val();
+        var Add_Info_3=$("#Add_Info").val();
 
-        //金额转换为大写
-        var Money=$("#Money").val();       
-        parseFloat(Money);
+        var Bill_Num_3=$("#Bill_Num").val();
+        var Bill_Count_3=$("#Bill_Count").val();
+        var Bill_Type_3=$("#BillSpec").val();
+
+        var Money_Arab_3=$("#Money_Arab").val(); 
+        
+        $("#Bill_Num_3").text(Bill_Num_3);
+        $("#Bill_Count_3").text(Bill_Count_3);
+        $("#Bill_Type_3").text(Bill_Type_3);
+        $("#Money_Arab_3").text(Money_Arab_3);
+       
+        //转账金额转换为大写
+        parseFloat(Money_Arab_3);
 
         $("#print").jqprint({});
 
